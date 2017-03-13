6699a514ca889af168cdd5af134c1840fac321f4
Tingxuan <892339906@qq.com>
2017-03-14 00:24:04 +0800

    e

--- email.py
+++ email.py
@@ -6,17 +6,17 @@ from email.mime.text import MIMEText
 sender = 'from@runoob.com'
 receivers = ['892339906@qq.com']
 
-message = MIMEText('Python 发送邮件测试','plain','utf-8')
-message['From'] = Header("菜鸟教程",'utf-8')
-messsage['To'] = Header("测试",'utf-8')
+message = MIMEText('Python 发送邮件测试', 'plain', 'utf-8')
+message['From'] = Header("菜鸟教程", 'utf-8')
+messsage['To'] = Header("测试", 'utf-8')
 
 subject = 'Python SMTP 邮件测试'
-message['subject'] = Header(subject,'utf-8')
+message['subject'] = Header(subject, 'utf-8')
 
 
 try:
     smtpObj = smtplib.SMTP('localhost')
     ssmtpObj.sendmail(sender, receivers, message.as_string())
-    print ("邮件发送成功")
+    print("邮件发送成功")
 except smtplib.SMTPException:
-    print ("Error:无法发送邮件") 
+    print("Error:无法发送邮件")
