e36eba86ba85d53a857a1e1fdd76ee56ee52c300
Tingxuan <892339906@qq.com>
2017-03-17 13:56:34 +0800

    3.17

--- emai_demol.py
+++ emai_demol.py
@@ -1,24 +1,22 @@
+# -*- coding: UTF-8 -*-
 import smtplib
-
 from email.mime.text import MIMEText
-from email.utils import formataddr
 from email.header import Header
 
 
-mail_host = 'smtp.163.com'
-mail_user = 'zhangtingxuan'
-mail_pass = 'ztx123456'
-
+mail_host = "smtp.163.com"
+mail_user = "zhangtingxuan"
+mail_pass = "ztx123456"
 
 sender = 'zhangtingxuan@163.com'
-receivers = ['kamitake@2980.com']
+receivers = '892339906@qq.com'
 
 message = MIMEText('Python 发送邮件测试', 'plain', 'utf-8')
 message['From'] = Header("菜鸟教程", 'utf-8')
 message['To'] = Header("测试", 'utf-8')
 
 subject = 'Python SMTP 邮件测试'
-message['subject'] = Header(subject, 'utf-8')
+message['Subject'] = Header(subject, 'utf-8')
 
 
 try:
--- test2.py
+++ test2.py
@@ -1,166 +1,156 @@
-#!/usr/bin/env python  
-# coding=utf-8  
+#!/usr/bin/env python 
+# coding=utf-8 
 
 
 import smtplib
-import Tkinter
-import tkMessageBox  
-import string  
+from tkinter import *
+import tkinter.messagebox
+import string
 
 
-class loginPage(object):
+class loginPage(object):
 
-    def __init__(self, master, info='Mail Send System'):
+    def __init__(self, master, info='Mail Send System'):
 
-        self.master = master  
-        self.mainlabel = Label(master, text=info, justify=CENTER)  
-        self.mainlabel.grid(row=0, columnspan=3)  
+        self.master = master
+        self.mainlabel = Label(master, text=info, justify=CENTER)
+        self.mainlabel.grid(row=0, columnspan=3)
 
+        self.user = Label(master, text='username', borderwidth=2)
+        self.user.grid(row=1, sticky=W)
 
-        self.user = Label(master, text='username', borderwidth=2)  
-        self.user.grid(row=1, sticky=W)  
+        self.pwd = Label(master, text='password', borderwidth=2)
+        self.pwd.grid(row=2, sticky=W)
 
+        self.userEntry = Entry(master)
+        self.userEntry.grid(row=1, column=1, columnspan=2)
+        self.userEntry.focus_set()
 
-        self.pwd = Label(master, text='password', borderwidth=2)  
-        self.pwd.grid(row=2, sticky=W)  
+        self.pwdEntry = Entry(master, show='*')
+        self.pwdEntry.grid(row=2, column=1, columnspan=2)
 
+        self.loginButton = Button(
+            master, text='Login', borderwidth=2, command=self.login)
+        self.loginButton.grid(row=3, column=1)
 
-        self.userEntry = Entry(master)  
-        self.userEntry.grid(row=1, column=1, columnspan=2)  
-        self.userEntry.focus_set()  
+        self.clearButton = Button(
+            master, text='Clear', borderwidth=2, command=self.clear)
+        self.clearButton.grid(row=3, column=2)
 
+    def login(self):
 
-        self.pwdEntry = Entry(master, show='*')  
-        self.pwdEntry.grid(row=2, column=1, columnspan=2)  
+        self.username = self.userEntry.get().strip()
+        self.passwd = self.pwdEntry.get().strip()
+        if len(self.username) == 0 or len(self.passwd) == 0 or '@' not in self.username:
+            tkinter.messagebox.showwarning('警告', '用户名或者密码为空或邮件格式不正确')
+            self.clear()
+            self.userEntry.focus_set()
+        return
+        self.getSmtpHost()
+        self.connect()
 
+    def connect(self):
+        'this method will try to connet the SMTP server according the current user'
+        HOST = 'smtp.' + self.smtp + '.com'
+        try:
 
-        self.loginButton = Button(master, text='Login', borderwidth=2, command=self.login)  
-        self.loginButton.grid(row=3, column=1)  
 
+            self.mySMTP = SMTP(HOST)
+            self.mySMTP.login(self.username, self.passwd)
+        except Exception as e:
 
-        self.clearButton = Button(master, text='Clear', borderwidth=2, command=self.clear)  
-        self.clearButton.grid(row=3, column=2)  
+            tkMessageBox.showerror('连接错误', '%s' % e)
+            return
+        self.mySendMail = sendMail(self.master, self.mySMTP, self.username)
 
-    def login(self):
+    def clear(self):
 
-        self.username = self.userEntry.get().strip()  
-        self.passwd = self.pwdEntry.get().strip()  
-    if len(self.username) == 0 or len(self.passwd) == 0 or '@' not in self.username:
+        self.userEntry.delete(0, END)
+        self.pwdEntry.delete(0, END)
 
-        tkMessageBox.showwarning('警告', '用户名或者密码为空或邮件格式不正确')  
+    def getSmtpHost(self):
 
+        'this method try to obtian the SMTP HOST according the user account' 
+        firstSplit = self.username.split('@')[1] 
+        self.smtp = firstSplit.split('.')[0] 
 
-            self.clear()  
-            self.userEntry.focus_set()  
-            return   
 
+class sendMail(object):
 
-        self.getSmtpHost()  
-        self.connect()  
+    'my sendemail class' 
+    def __init__(self, master, smtp='', sender=''):
 
-     def connect(self):
+        self.smtp = smtp 
+        self.sender = sender 
 
-        'this method will try to connet the SMTP server according the current user'  
-        HOST = 'smtp.' + self.smtp + '.com'  
-         try:
 
-            self.mySMTP = SMTP(HOST)  
-            self.mySMTP.login(self.username, self.passwd)  
-        # except SMTPConnectError:  
-         except Exception, e:
+        self.sendPage = Toplevel(master) 
 
-            tkMessageBox.showerror('连接错误', '%s' % e)  
-             return   
-        self.mySendMail = sendMail(self.master, self.mySMTP, self.username)  
 
-     def clear(self):
+        self.sendToLabel = Label(self.sendPage, text='send to:') 
+        self.sendToLabel.grid() 
+        self.sendToEntry = Entry(self.sendPage) 
+        self.sendToEntry.grid(row=0, column=1) 
 
-        self.userEntry.delete(0, END)  
-        self.pwdEntry.delete(0, END)  
 
-     def getSmtpHost(self):
+        self.subjectLabel = Label(self.sendPage, text='subject:') 
+        self.subjectLabel.grid(row=1, column=0) 
+        self.subjectEntry = Entry(self.sendPage) 
+        self.subjectEntry.grid(row=1, column=1) 
 
-        'this method try to obtian the SMTP HOST according the user account'  
-        firstSplit = self.username.split('@')[1]  
-        self.smtp = firstSplit.split('.')[0]  
 
+        self.fromToLabel = Label(self.sendPage, text='from to:') 
+        self.fromToLabel.grid(row=2, column=0) 
+        self.formToAdd = Label(self.sendPage, text=self.sender) 
+        self.formToAdd.grid(row=2, column=1) 
 
-class sendMail(object):
 
-    'my sendemail class'  
-     def __init__(self, master, smtp='', sender=''):
+        self.sendText = Text(self.sendPage) 
+        self.sendText.grid(row=3, column=0, columnspan=2) 
 
-        self.smtp = smtp  
-        self.sender = sender  
 
+        self.sendButton = Button(self.sendPage, text='send', command=self.sendMail) 
+        self.sendButton.grid(row=4, column=0) 
 
-        self.sendPage = Toplevel(master)  
 
+        self.newButton = Button(self.sendPage, text='new mail', command=self.newMail) 
+        self.newButton.grid(row=4, column=1) 
 
-        self.sendToLabel = Label(self.sendPage, text='send to:')  
-        self.sendToLabel.grid()  
-        self.sendToEntry = Entry(self.sendPage)  
-        self.sendToEntry.grid(row=0, column=1)  
+    def getMailInfo(self):
 
+        self.sendToAdd = self.sendToEntry.get().strip() 
+        self.subjectInfo = self.subjectEntry.get().strip() 
+        self.sendTextInfo = self.sendText.get(1.0, END) 
 
-        self.subjectLabel = Label(self.sendPage, text='subject:')  
-        self.subjectLabel.grid(row=1, column=0)  
-        self.subjectEntry = Entry(self.sendPage)  
-        self.subjectEntry.grid(row=1, column=1)  
+    def sendMail(self):
 
+        self.getMailInfo() 
+        body = string.join(("From: %s" % self.sender, "To: %s" % self.sendToAdd, "Subject: %s" % self.subjectInfo, "", self.sendTextInfo), "\r\n") 
+        try:
 
-        self.fromToLabel = Label(self.sendPage, text='from to:')  
-        self.fromToLabel.grid(row=2, column=0)  
-        self.formToAdd = Label(self.sendPage, text=self.sender)  
-        self.formToAdd.grid(row=2, column=1)  
+            self.smtp.sendmail(self.sender, [self.sendToAdd], body) 
+        except Exception as e:
 
+            tkMessageBox.showerr('发送失败', "%s" % e) 
+            return  
+            tkMessageBox.showinfo('提示', '邮件已发送成功！') 
 
-        self.sendText = Text(self.sendPage)  
-        self.sendText.grid(row=3, column=0, columnspan=2)  
+    def newMail(self):
 
+        self.sendToEntry.delete(0, END) 
+        self.subjectEntry.delete(0, END) 
+        self.sendText.delete(1.0, END) 
 
-        self.sendButton = Button(self.sendPage, text='send', command=self.sendMail)  
-        self.sendButton.grid(row=4, column=0)  
 
+if __name__ == '__main__':
 
-        self.newButton = Button(self.sendPage, text='new mail', command=self.newMail)  
-        self.newButton.grid(row=4, column=1)  
 
-     def getMailInfo(self):
+    root = Tk() 
+    root.title('简易发送邮件程序') 
 
-        self.sendToAdd = self.sendToEntry.get().strip()  
-        self.subjectInfo = self.subjectEntry.get().strip()  
-        self.sendTextInfo = self.sendText.get(1.0, END)  
 
-     def sendMail(self):
+    myLogin = loginPage(root) 
 
-        self.getMailInfo()  
-        body = string.join(("From: %s" % self.sender, "To: %s" % self.sendToAdd, "Subject: %s" % self.subjectInfo, "", self.sendTextInfo), "\r\n")  
-         try:
 
-            self.smtp.sendmail(self.sender, [self.sendToAdd], body)  
-         except Exception, e:
-
-            tkMessageBox.showerr('发送失败', "%s" % e)  
-             return   
-        tkMessageBox.showinfo('提示', '邮件已发送成功！')  
-
-     def newMail(self):
-
-        self.sendToEntry.delete(0, END)  
-        self.subjectEntry.delete(0, END)  
-        self.sendText.delete(1.0, END)  
-
-
-if __name__ == '__main__':
-
-
-    root = Tk()  
-    root.title('简易发送邮件程序')  
-
-
-    myLogin = loginPage(root)  
-
-
-    # root.wait_window(myLogin.mySendMail.sendPage)  
-    mainloop()  
+ # root.wait_window(myLogin.mySendMail.sendPage) 
+    mainloop() 
